0677ea907ec8195547105644479a8d84b321e942
Petteri Mattila <petterimattila@protonmail.com>
2024-01-31 10:32:11 +0200

    Base

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+        <h1>ML enhanced fitness</h1>
+        <div>
+            <h2>Source</h2>
+            <video id="source" src=""></video>
+        </div>
+        <div>
+            <h2>Result</h2>
+            <canvas id="result"></canvas>
+        </div>
+    </>
+  )
+}
+
+export default App
