c4a85e760e6d232cca82d37cfeb9e119a46dbdb0
Petteri Mattila <petterimattila@protonmail.com>
2024-02-01 17:04:48 +0200

    Switching to BlazePose from PoseNet

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,12 @@ import * as poseDetection from "@tensorflow-models/pose-detection"
 import "@tensorflow/tfjs-backend-webgl"
 // Register one of the TF.js backends.
 
-// import videoSource from "../public/Nike Commercial - .mp4"
-import testPose from "../public/test-pose.jpg"
+import videoSource from "/Nike Commercial - .mp4"
+import testPose from "/test-pose.jpg"
 import { useEffect } from "react"
 
 async function trackPose(sourceElementId: string): Promise<poseDetection.Pose[]> {
-    const config: poseDetection.MoveNetModelConfig = {
-        enableSmoothing: true,
-        modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING
-    }
-
-    const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, config)
+    const detector = await poseDetection.createDetector(poseDetection.SupportedModels.BlazePose, { runtime: "tfjs", modelType: "lite" })
     const element: PixelInput = document.getElementById(sourceElementId) as HTMLImageElement | HTMLVideoElement
     const poses = await detector.estimatePoses(element)
 
@@ -29,9 +24,9 @@ async function trackPose(sourceElementId: string): Promise<poseDetection.Pose[]>
     )
 }
 
-function visualizePose(poses: poseDetection.Pose[]) {
+function visualizePose(poses: poseDetection.Pose[], resultId: string) {
     const { keypoints, score } = poses[0] // Expecting only one person.
-    const canvas = document.querySelector("#result") as HTMLCanvasElement
+    const canvas = document.getElementById(resultId) as HTMLCanvasElement
     const context = canvas.getContext("2d")!
 
     if (score! < 0.3) return
@@ -62,8 +57,9 @@ type Bodyparts = {
 
 function categorializeJoints(array: Bodyparts): Bodyparts {
     const length = array.unordered.length
-    const arm = array.unordered.splice(0, length / 2)
-    const leg = array.unordered.splice(0, length / 2)
+    // Switched to BlazePose, where arms and legs have different amount of "joint points"
+    const arm = array.unordered.splice(0, (length / 2) + 1)
+    const leg = array.unordered.splice(0, (length / 2) - 1)
     array.arm = arm
     array.leg = leg
 
@@ -72,7 +68,7 @@ function categorializeJoints(array: Bodyparts): Bodyparts {
 
 function connectHotspots(keypoints: poseDetection.Keypoint[], context: CanvasRenderingContext2D) {
     // Connecting dots is easier when they are in the correct order, separating left and right sides as well as the head
-    const remainingKeypoints = keypoints.slice(5, keypoints.length)
+    const remainingKeypoints = keypoints.slice(11, keypoints.length)
     // const head = keypoints.slice(0, 5)
     let leftSide: Bodyparts = { arm: [], leg: [], unordered: [] }
     let rightSide: Bodyparts = { arm: [], leg: [], unordered: [] }
@@ -100,9 +96,9 @@ function connectHotspots(keypoints: poseDetection.Keypoint[], context: CanvasRen
             // i + 1 prevents out of bounds error
             for (let i = 0; i + 1 < array.length; i++) {
                 // Maybe useful later, if I want to color the line based on the score
-                // const score01 = array[i]?.score
-                // const score02 = array[i + 1]?.score
-                // const color = pickColor((score01! < score02!) ? score01 : score02)
+                // const score01 = array[i].score
+                // const score02 = array[i + 1].score
+                // const color = pickColor((score01 < score02) ? score01 : score02)
                 const { x: startX, y: startY } = array[i]
                 const { x: endX, y: endY } = array[i + 1]
 
@@ -125,26 +121,48 @@ function pickColor(score: number | undefined): string {
     else return "black"
 }
 
+function hookPoseVisualizerToVideo() {
+    trackPose("video-source")
+        .then((poses: poseDetection.Pose[]): void => {
+            visualizePose(poses, "video-result")
+        })
+        .catch((rejected): void => {
+            if (typeof rejected === "number")
+                console.warn("Couldn't track a pose from video. Pose status:", rejected)
+            else 
+                console.warn("Possible error occured during video pose tracking.", rejected as Error)
+        })
+}
+
 function App() {
     useEffect(() => {
-        trackPose("source")
+        trackPose("image-source")
             .then((poses: poseDetection.Pose[]): void => {
-                visualizePose(poses)
+                visualizePose(poses, "image-result")
             })
             .catch((rejected): void => {
                 if (typeof rejected === "number")
-                    console.warn("Couldn't track a pose. Pose status:", rejected)
+                    console.warn("Couldn't track a pose from image. Pose status:", rejected)
                 else 
-                    console.warn("Possible error occured.", rejected as Error)
+                    console.warn("Possible error occured during image pose tracking.", rejected as Error)
             })
     })
 
     return (
         <>
-            <h1>ML enhanced fitness</h1>
+            <h1>Tracking poses</h1>
+            <h2>Image source</h2>
             <div className="source-container">
-                <img id="source" src={ testPose } width={ 355.55 } height={ 200 }></img>
-                <canvas id="result" width={ 355.55 } height={ 200 }></canvas>
+                <img id="image-source" src={ testPose } width={ 355.55 } height={ 200 }></img>
+                <canvas id="image-result" className="result" width={ 355.55 } height={ 200 }></canvas>
+            </div>
+            <h2>Video source</h2>
+            <div className="source-container">
+                <video id="video-source" width={ 355.55 } height={ 200 } typeof="video/mp4" autoPlay muted src={ videoSource }
+                    onPlay={ hookPoseVisualizerToVideo }>
+                    <p>Sorry, this browser doesn't support HTML videos.</p>
+                </video>
+                <canvas id="video-result" className="result" width={ 355.55 } height={ 200 }></canvas>
             </div>
         </>
     )
