66ea743f1ae79a381cff09968f2af13558da5cc2
Petteri Mattila <petterimattila@protonmail.com>
2024-02-09 10:26:50 +0200

    Restructuring

--- src/lib/detection/poseHook.ts
+++ src/lib/detection/poseHook.ts
@@ -1,6 +1,6 @@
-import { visualizePose } from "../lib/poseVisualizer"
+import { visualizePose } from "../poseVisualizer"
 import { PixelInput } from "@tensorflow-models/pose-detection/dist/shared/calculators/interfaces/common_interfaces"
-import { trackPose } from "../lib/poseDetection"
+import { trackPose } from "./poseDetection"
 import * as poseDetection from "@tensorflow-models/pose-detection"
 
 export function hookPoseVisualizerToVideo(elementId: string,source: PixelInput, detector: poseDetection.PoseDetector, frameRate: number = 20): number | null {
