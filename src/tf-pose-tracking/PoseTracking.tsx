a1fdfa9c22e6567a75a58e69af0f806f2dc194cf
Petteri Mattila <petterimattila@protonmail.com>
2024-02-09 10:07:01 +0200

    Commenting hand tracking out for now

--- src/tf-pose-tracking/PoseTracking.tsx
+++ src/tf-pose-tracking/PoseTracking.tsx
@@ -1,18 +1,18 @@
 import { useEffect, useRef, useState } from "react"
 import * as poseDetection from "@tensorflow-models/pose-detection"
-import * as handDetector from "@tensorflow-models/hand-pose-detection"
+// import * as handDetector from "@tensorflow-models/hand-pose-detection"
 import { setupBodyDetector } from "../lib/poseDetection"
-import { setupHandDetector } from "../lib/handPoseDetection"
+// import { setupHandDetector } from "../lib/handPoseDetection"
 import Webcam from "../webcam/Webcam"
 import { hookPoseVisualizerToVideo } from "../lib/poseHook"
 
-interface detectors {
-    body: poseDetection.PoseDetector | null
-    hands: handDetector.HandDetector | null
-}
+// interface detectors {
+//     body: poseDetection.PoseDetector | null
+//     hands: handDetector.HandDetector | null
+// }
 
 function PoseTracking() {
-    const detectors = useRef<detectors>()
+    const detectors = useRef<poseDetection.PoseDetector | null>(null)
     const webcamElement = useRef<HTMLVideoElement | null>(null)
     const [visualizerHook, setVisualizerHook] = useState<number | null>(null)
 
@@ -20,24 +20,18 @@ function PoseTracking() {
         if (!detectors.current) {
             setupBodyDetector()
                 .then((poseDetector) => {
-                    detectors.current!.body = poseDetector
-                })
-
-            setupHandDetector
-                .then((handDetector) => {
-                    detectors.current!.hands = handDetector
+                    detectors.current = poseDetector
                 })
         }
 
-        if (!visualizerHook && detectors.current?.body && webcamElement.current) {
-            const hook = hookPoseVisualizerToVideo("webcam-result", webcamElement.current, detectors.current.body)
+        if (!visualizerHook && detectors.current && webcamElement.current) {
+            const hook = hookPoseVisualizerToVideo("webcam-result", webcamElement.current, detectors.current)
             setVisualizerHook(hook)
         }
     }, [detectors, visualizerHook])
 
     return (
         <div className="source-container">
-            <h2>Web cam source</h2>
             <Webcam width={ 500 } height={ 375 } webcamVideo={ webcamElement } />
             <canvas id="webcam-result" width={ 500 } height={ 375 }></canvas>
         </div>
