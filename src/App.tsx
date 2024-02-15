11d70b97c0ecdec2f18cfa3d283511f5c5ff35d8
Petteri Mattila <petterimattila@protonmail.com>
2024-02-15 15:58:58 +0200

    Base menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
+import ManageExcercisesMenu from "./pages/manage-excercises/ManageExcercisesMenu"
+import PoseTracking from "./pages/tf-pose-tracking/PoseTracking"
+import { useState, createContext, useContext } from "react"
 import "./App.css"
-import PoseTracking from "./tf-pose-tracking/PoseTracking"
+
+const MenuContext = createContext<number>(0)
 
 function App() {
+    const [menu, setMenu] = useState(0)
+
+    return (
+        <MenuContext.Provider value={ menu }>
+            <Back set={ setMenu } />
+
+            { (menu === 0) && (
+                <>
+                <h1>Menu</h1>
+                <button onClick={ () => setMenu(1) }>Pose tracking</button>
+                <button onClick={ () => setMenu(2) }>Manage excercises</button>
+                </>
+            ) }
+
+            { (menu === 1) && (
+                <PoseTracking />
+            ) }
+
+            { (menu === 2) && (
+                <ManageExcercisesMenu />
+            ) }
+        </MenuContext.Provider>
+    )
+}
+
+type BackProps = {
+    set: React.Dispatch<React.SetStateAction<number>>
+}
+
+function Back({ set }: BackProps) {
+    const context = useContext(MenuContext)
+
     return (
         <>
-        <PoseTracking />
+        { (context !== 0) &&
+            <button onClick={ () => set(0) }>Back</button>
+        }
         </>
     )
 }
