19b8054c27ee2fb2e18c3cadde998d0bbfa1d64c
Petteri Mattila <petterimattila@protonmail.com>
2024-02-16 15:58:43 +0200

    Types used with local storage

---
+++ src/lib/types/excerciseTypes.ts
@@ -0,0 +1,20 @@
+export type Excercise = {
+    name: string
+    type: string
+    moves: Move[]
+    previousRecords: Record[]
+    completionCounter: number
+}
+
+type Move = {
+    name: string
+    sets: string
+    repsInSet?: number | undefined
+    breakBetweenSets?: number | undefined
+    personalBestReps?: number | undefined
+}
+
+type Record = Move & {
+    setCount: number
+    averageRepCount: number
+}
